fb18147fbac01570c40b70e5a21290ec0ca81f8f
yodevhail <yodevhail@gmail.com>
2018-10-03 15:22:40 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args){
+
+
+            int mynumber = 2;
+            double myothernumber = 1.7938;
+
+            System.out.println(mynumber);
+            System.out.println(myothernumber);
+	// write your code here
+    }
+}
